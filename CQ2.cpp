d244629c0b75d3c4995da7f80f8ed394341519a0
Aryan Mavi <aryan009m@gmail.com>
2026-08-18 11:40:39 +0530

    using LL

---
+++ CQ2.cpp
@@ -0,0 +1,43 @@
+// #include<bits/stdc++.h>
+
+// using namespace std;
+
+// class Node{
+//     public:
+//     int data;
+//     Nod*next;
+//     Node(int value){
+//         data = value;
+//         next = null;
+//     }
+// };
+
+// class Queue{
+//     Node*rear,*front;
+//     Queue(){
+//         rear = null;
+//         front = null;
+//     }
+//     void enqueue(int value){
+//         Node*newNode = new Node(value);
+//         if( front == null){
+//             front = rear = newNode;
+//         }
+//         else{
+//             rear->next = newNode;
+//             rear = newNode;
+//         }
+        
+
+//     }
+
+//     void dequeue(){
+
+//     }
+// }
+
+
+// int main(){
+
+// return 0;
+// }
